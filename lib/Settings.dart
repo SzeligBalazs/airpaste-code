a8558cb6e7bbe4efd886be984273b7f920ea93e6
Szélig Balázs <80389103+SzeligBalazs@users.noreply.github.com>
2022-03-16 20:16:13 +0100

    Added source code

---
+++ lib/Settings.dart
@@ -0,0 +1,132 @@
+import 'package:airpaste/About.dart';
+import 'package:airpaste/Home.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:r_get_ip/r_get_ip.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  saveSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('ipaddress', ipText.text);
+  }
+
+  getValues() async {
+    final prefs = await SharedPreferences.getInstance();
+    ipText.text = prefs.getString('ipaddress');
+    ipText.text = prefs.getString('ipQR');
+  }
+
+  @override
+  void initState() {
+    getValues();
+    super.initState();
+  }
+
+  TextEditingController ipText = TextEditingController();
+  String ipQR = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('Settings'),
+        elevation: 0,
+        foregroundColor: Color(0xFF121212),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => About()),
+                );
+              },
+              child: Text('About',
+                  style: TextStyle(
+                      color: Color(0xFF121212), fontWeight: FontWeight.bold))),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Connect to a device (IP address)\n",
+                style: TextStyle(
+                    color: Color(0xFF121212), fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                style: const TextStyle(color: Color(0xFF121212)),
+                controller: ipText,
+                decoration: InputDecoration(
+                    filled: true,
+                    hintStyle: const TextStyle(color: Color(0xFF121212)),
+                    hintText: 'Type the IP address',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        ipText.clear();
+                      },
+                      icon: const Icon(Icons.clear),
+                    ),
+                    fillColor: const Color(0xFFeeeeee)),
+              ),
+              SizedBox(
+                height: 128,
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Your internal IP address is (Server IP) ',
+                      style: TextStyle(
+                        color: Color(0xFF121212),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    FutureBuilder(
+                        future: RGetIp.internalIP,
+                        builder: (context, snapshot) {
+                          return SelectableText(
+                            '${snapshot.hasData ? snapshot.data : "0.0.0.0"}',
+                            style: TextStyle(
+                              color: Color(0xFF121212),
+                              fontSize: 26,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        }),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          saveSettings();
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+        },
+        icon: Icon(Icons.done_all),
+        label: Text(
+          'Save',
+        ),
+        backgroundColor: Color(0xFF121212),
+        foregroundColor: Colors.white,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:airpaste/About.dart';
+import 'package:airpaste/Home.dart';
+import 'package:airpaste/Settings.dart';
+import 'package:airpaste/receive/Receive.dart';
+import 'package:airpaste/send/Send.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_background/flutter_background.dart';
+
+void main() async {
+  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  final androidConfig = FlutterBackgroundAndroidConfig(
+    notificationTitle: "Background process",
+    notificationText: "Enable this to run the app in the background",
+    notificationImportance: AndroidNotificationImportance.Default,
+    notificationIcon: AndroidResource(
+        name: 'icon_rounded',
+        defType: 'drawable'), // Default is ic_launcher from folder mipmap
+  );
+  bool success =
+      await FlutterBackground.initialize(androidConfig: androidConfig);
+
+  bool successfull = await FlutterBackground.enableBackgroundExecution();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "home": (context) => HomePage(),
+        "/settings": (context) => Settings(),
+        "/about": (context) => About(),
+        "/send": (context) => Send(),
+        "/recieve": (context) => Receive(),
+      },
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
